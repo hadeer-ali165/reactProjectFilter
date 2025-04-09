ee1b700edeffc270c9912f9fb87e55e474f6549d
Hadeer <hadeeraly165@gmail.com>
2025-04-09 17:07:49 +0200

    delete react reveal

--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -1,8 +1,7 @@
 import React, { useState } from 'react'
 import style from './Product.module.css'
 import { IoMdClose } from "react-icons/io";
-import Zoom from 'react-reveal/Zoom';
-import Shake from 'react-reveal/Shake';
+
 const Products = ({product}) => {
     let [statePupup , setStatePupp] = useState(null)
     const handleClickPop = (el)=>{
@@ -14,7 +13,6 @@ const Products = ({product}) => {
 return (
     <>
     <div className={`${style.parent} mt-8 flex flex-wrap justify-between`}>
-        <Zoom>
         {product.map((el)=>
         <div onClick={()=> handleClickPop(el)} className={`${style.box} w-[45%] bg-[#fff] mt-5 flex items-center rounded-[10px] overflow-hidden cursor-pointer`}>
             <div className="txts w-[50%] ps-5">
@@ -24,11 +22,9 @@ return (
         <img src={el.img} className='w-[50%] h-[200px]' alt="" />
         </div>
         )}
-        </Zoom>
     </div>
 
     {statePupup && (
-        <Shake>
         <div className={`${style.pupp} fixed top-[17%] mx-auto left-[10%] right-[10%] flex w-[50%] mx-auto`}>
             <p className='flex justify-end pe-5 cursor-pointer text-[20px] font-bold' onClick={handleclcikDeleted}><IoMdClose /></p>
         <div className={`${style.boxs} w-[70%] sm:w-[100%] w-[100%] md:w-[80%] lg:w-[70%] bg-[#333131a3] mx-auto rounded-[10px] text-[#fff] text-center overflow-hidden`}>
@@ -36,7 +32,6 @@ return (
             <p className='py-5'>{statePupup.title}</p>
         </div>
     </div>
-    </Shake>
     )}
     </>
 )
